389e6948b36fc298becf26e51dcc8b676cd319e6
Tim Abell <tim@timwise.co.uk>
2014-11-04 16:27:41 +0000

    stub trim class

--- trim.cs
+++ trim.cs
@@ -5,8 +5,17 @@ namespace SqlTrimmer
 	{
 		public static int Main()
 		{
-			System.Console.Write("helloworld");
+			var e = new Engine9();
+			e.Trim("test.sql");
 			return 0; // all good
+		}
+	}
+
+	public class Engine9
+	{
+		public void Trim(string path)
+		{
+			System.Console.Write("trimming " + path + "\n");
 		}
 	}
 }
